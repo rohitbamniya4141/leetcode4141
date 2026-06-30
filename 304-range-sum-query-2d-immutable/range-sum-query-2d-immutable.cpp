4b75474b3b10819ebb340fcc0d6c33f7bcbbf266
Rohit Bamniya <rohitbamniya4141@gmail.com>
2026-06-30 16:53:58 +0530

    Time: 16 ms (87.03%) | Memory: 149.1 MB (89.87%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.cpp
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.cpp
@@ -6,28 +6,19 @@ public:
         int m = matrix.size();
         int n = matrix[0].size();
 
-        dp.assign(m, vector<int>(n, 0));
+        dp.assign(m+1, vector<int>(n+1, 0));   //dp m+1, n+1 size ki hai mtlb dp[3][4] ka mtlb (0,0) se mat[2][3] ka sum kitna hai
 
-        for(int i = 0; i < matrix.size(); i++){
-            int sum = 0;
-            for(int j = 0; j < matrix[i].size(); j++){
-                sum+= matrix[i][j];
-                dp[i][j] = sum;
+        for(int i = 1; i <= m; i++){
+            for(int j = 1; j <= n; j++){
+                int curr = matrix[i-1][j-1], top = dp[i-1][j], left = dp[i][j-1], topleft = dp[i-1][j-1];
+                dp[i][j] = curr + top + left -(topleft);
             }
         }
     }
     
     int sumRegion(int row1, int col1, int row2, int col2) {
-        int s = 0;
-        for(int i = row1; i <= row2; i++){
-            if(col1 > 0){
-                s+=(dp[i][col2] - dp[i][col1-1]);
-            }
-            else{
-                s+= dp[i][col2];
-            }
-        }
-        return s;
+        int curr = dp[row2+1][col2+1], top = dp[row1][col2+1], left = dp[row2+1][col1], topleft = dp[row1][col1];
+        return curr-top-left+topleft;
     }
 };
 
