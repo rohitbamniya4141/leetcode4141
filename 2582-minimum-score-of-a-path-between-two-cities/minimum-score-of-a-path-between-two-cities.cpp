7baa160c5a620efd5572bfd3493f2fb4d7bb1279
Rohit Bamniya <rohitbamniya4141@gmail.com>
2026-07-04 10:57:30 +0530

    Time: 60 ms (58.89%) | Memory: 132.6 MB (77.98%) - LeetSync

--- 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.cpp
+++ 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.cpp
@@ -2,8 +2,7 @@ class Solution {
 public:
     int minScore(int n, vector<vector<int>>& roads) {
         
-        unordered_map<int,vector<pair<int,int>>>adj;
-        int last =1;
+        vector<vector<pair<int,int>>>adj(n+1);
         for(int i = 0; i < roads.size(); i++){
             int src = roads[i][0];
             int dest = roads[i][1];
@@ -12,19 +11,18 @@ public:
             adj[dest].push_back({src,w});
         }
         int ans = INT_MAX;
-        vector<bool>vis(n+1,0);
+        vector<bool>vis(n+1, false);
         queue<int>q;
-        vis[1]=1;
         q.push(1);
         while(!q.empty()){
             int curr = q.front();
             q.pop();
-            for(auto d : adj[curr]){
-                ans = min(ans,d.second);
-                if(!vis[d.first]){
-                    q.push(d.first);
-                    vis[d.first] = 1;
+            for(int i = 0; i < adj[curr].size(); i++){
+                if(!vis[adj[curr][i].first]) {
+                    q.push(adj[curr][i].first);
+                    vis[(adj[curr][i].first)] = true;
                 }
+                ans = min(adj[curr][i].second, ans);
             }
         }
     return ans;
