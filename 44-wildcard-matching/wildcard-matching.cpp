feaf5d1cf1055c2bd9fb6e00f0264cf9170377a5
Rohit Bamniya <rohitbamniya4141@gmail.com>
2026-07-06 12:17:49 +0530

    Time: 51 ms (54.82%) | Memory: 54.6 MB (22.20%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -25,7 +25,32 @@ public:
     }
     bool isMatch(string s, string p) {
         int m = s.size(), n = p.size();
-        vector<vector<int>>dp(m, vector<int>(n,-1));
+        vector<vector<int>>dp(m+1, vector<int>(n+1,-1));
         return solve(s, p, s.size()-1, p.size()-1, dp);
+
+        for(int i = m-1; i >= 0 ; i--){
+            dp[i][n] = false;
+        }
+        for(int j = n-1; j >= 0 ;j--){
+            if(p[j] == '*'){
+                dp[m][j] = dp[m][j+1];
+            }
+            else dp[m][j] = false;
+        }
+        dp[m][n]= true;
+        for(int i = m-1; i >= 0; i--){
+            for(int j = n-1; j >=0; j--){
+                if((s[i] == p[j]) || (p[j] == '?')){
+                    dp[i][j] = dp[i+1][j+1];
+                }
+                else if(p[j] == '*'){
+                    bool c1 = dp[i+1][j];
+                    bool c2 = dp[i][j+1];
+                    dp[i][j] = c1||c2;
+                }
+                else dp[i][j] = false;
+            }
+        }
+        return dp[0][0];
     }
 };
