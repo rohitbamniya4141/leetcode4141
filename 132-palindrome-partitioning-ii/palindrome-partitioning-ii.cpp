a41bdd20efb7e04a9b05337d57ef7131f5e807cc
Rohit Bamniya <rohitbamniya4141@gmail.com>
2026-07-07 20:23:00 +0530

    Time: 1860 ms (5.01%) | Memory: 12.3 MB (26.38%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -10,7 +10,7 @@ public:
         return true;
     }
 
-    int solve(string&s, int i, int j, vector<int>&dp){
+    int solve(string&s, int i, int j, vector<int>&dp, vector<vector<bool>>&pal){
         if(i >= j){
             return 0;
         }
@@ -20,8 +20,8 @@ public:
         if(dp[i] != -1) return dp[i];
         int ans = INT_MAX;
         for(int k = i; k <= j-1; k++){
-            if(ispalindrome(s, i, k)){
-                int p2 = solve(s, k+1, j, dp);
+            if(pal[i][k]){
+                int p2 = solve(s, k+1, j, dp, pal);
                 ans = min(ans, p2 + 1);
             }
         }
@@ -29,6 +29,14 @@ public:
     }
     int minCut(string s) {
         vector<int>dp(s.size(), -1);
-        return solve(s, 0, s.size()-1, dp);
+        vector<vector<bool>>pal(s.size(), vector<bool>(s.size(), false));
+        for(int i = 0; i < s.size(); i++){
+            for(int j = i; j < s.size(); j++){
+                if(ispalindrome(s, i, j)){
+                    pal[i][j] = true;
+                }
+            }
+        }
+        return solve(s, 0, s.size()-1, dp, pal);
     }
 };
