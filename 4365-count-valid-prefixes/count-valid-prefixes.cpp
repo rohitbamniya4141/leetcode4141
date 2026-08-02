a325f1d7f4159c35dd724fd03a4288cdf170e52b
Rohit Bamniya <rohitbamniya4141@gmail.com>
2026-08-02 16:54:23 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (33.33%) - LeetSync

---
+++ 4365-count-valid-prefixes/count-valid-prefixes.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int countValidPrefixes(string s) {
+        int zero = 0, one = 0, cnt = 0;
+        for(char c : s){
+            if(c == '0') zero++;
+            else one++;
+            if(abs(zero-one) <= 1) cnt++;
+        }
+        return cnt;
+    }
+};
