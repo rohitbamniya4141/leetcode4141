a7674af1211368f699492130647df4daa0fdb7be
Rohit Bamniya <rohitbamniya4141@gmail.com>
2026-09-14 09:24:04 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (98.27%) - LeetSync

---
+++ 866-rectangle-overlap/rectangle-overlap.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool isRectangleOverlap(vector<int>& rec1, vector<int>& rec2) {
+        bool overlapX = min(rec1[2], rec2[2]) > max(rec1[0], rec2[0]);
+        bool overlapY = min(rec1[3], rec2[3]) > max(rec1[1], rec2[1]);
+
+        return overlapX && overlapY;
+    }
+};
